42b532833a4f94a5bc7dfb4e5135d8d2e5b7a3e6
Eike <jeikefriedrich@gmail.com>
2024-09-05 14:06:19 +0100

    remove test* files

--- bridge/test_module1_logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-#module_logger = logging.getLogger('main.module1')
-#module_logger = logging.getLogger('main')
-#module_logger.info(__name__ + ' imported')
-my_logger = logging.getLogger('my_app.module1')
-my_logger.info(__name__ + ' imported')
-
-def hello():
-    my_logger.info('hello')
