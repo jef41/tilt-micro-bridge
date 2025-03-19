05fe4b67033d155c28f4a8f75d5553f7ef05e711
Eike <jeikefriedrich@gmail.com>
2025-03-19 17:16:58 +0000

    display_driver: add extended info
    code tidied & simplified a little
    display will cycle through clock, SG & temp
    also if OG present display ABV & attenuation screens

--- bridge/lib/display_driver.py
+++ bridge/lib/display_driver.py
@@ -47,7 +47,7 @@ class LCD_Display:
         if not getattr(config, 'display_type', None):
             return None  # Prevent instance creation
         return super().__new__(cls)
-    
+
     def __init__(self, config: BridgeConfig):
         self.config = config
         self.lcd = None#getattr gpio pins
@@ -58,7 +58,7 @@ class LCD_Display:
         #update_frequency = 3 # seconds to cycle through each screen
         #self._check_for_display(pins) if (pins := getattr(config, 'lcd_spi_gpio', None)) else None
         self._check_for_display(display_type) if (display_type := getattr(self.config, 'display_type', None)) else None
-        
+
     def _check_for_display(self, display_type):
         #try:
         # print(f"***  {pins=}")
@@ -67,104 +67,29 @@ class LCD_Display:
         #self.lcd = PicoGraphics(display=DISPLAY_PICO_DISPLAY, pen_type=PEN_P4,rotate=0)
         self.lcd.set_backlight(getattr(self.config, 'lcd_backlight', 0.5))
         self.lcd.clear()
-        # print("brightness set")
-        #self.WIDTH, self.HEIGHT = self.lcd.get_bounds()
-        #print(f"some variables: {self.WIDTH}, {self.HEIGHT}, {getattr(self.config, 'lcd_brightness', 0.2)}")]
         # Create palette mapping dynamically
         self.colour_to_palette = {
             colour: self.lcd.create_pen(*get_color_values(colour, 1)) for colour in palette_cols
         }
 
-        #except Exception as e:
-        #    # print(f"error loading LCD GPIO pins {e}")
-        #    raise
-    
-        #def start_cards(colours, devices):
-        #    #
-        #    pass
-    
     async def card_stack(self, tilt_data_store: TiltHistory, cards: TiltDevice): #: TiltDevice
-        # TODO backlight issue - when called with custom SPI set_backlight is not available
-        # 		uncal values need scaling
-        #		tidy up
-        # better to look up config once & save value or look up each time?
-        # LED v bright at startup
-        # sort out waiting for data message - too big
-        # test not having SPI pins in config - no LCD loaded & no task?
+        # display the most recent data as basic & extended info for each tilt
         # some sort of loading screen
-        # handle extended info
         spacing = None
         sg, temp, uncal_sg, uncal_temp, rssi = None, None, None, None, None
-        msg=bytearray(12)
         self.lcd.set_font("serif")
-        #enabled_tilts.add("clock")
-        #self.cards.append(TiltDevice('clock'))
-        # TODO add a page for each Tilt that has OG, beer name
         while True:
             index = 0
-            WIDTH, HEIGHT = self.lcd.get_bounds()
             for tilt in cards:
-                #print(f"loop {enabled_colours}")
-                #if tilt_colour == "clock":
                 if index == 0:
                     # print("show clock")
-                    await self.update_clock()
-                #else:
-                # TODO move this to a def
-                # TODO some of this is static, calculate once & store
-                t_start = time.ticks_ms()
-                # TODO trap KeyError, log error & use white
-                # TODO build TiltDevice object - colour, rssi
-                #rssi = str(random.randint(10, 100)*-1)
-                uncal_temp, uncal_sg, tilt_values = self.read_latest_vals(tilt_data_store, tilt.colour)
-                n = 4 if tilt.hd else 3
-                self.lcd.set_pen(self.colour_to_palette["BG"])
-                self.lcd.clear()
-                self.lcd.set_thickness(3)
-                #lcd.text(text, x, y, wordwrap, scale, angle, spacing)
-                self.lcd.set_pen(self.colour_to_palette[tilt.colour.upper()])
-                if all([uncal_sg, uncal_temp, tilt_values]):
-                    msg=f"SG:{tilt_values.gravity:.{n}f}"
-                    self.lcd.text(msg, 0, 20, scale=1.5)
-                    offset=46
-                    temp = f"{tilt_values.temp_celsius:.1f}" if getattr(self.config, 'default_temp_unit') == "C" else f"{tilt_values.temp_fahrenheit:.1f}"
-                    self.lcd.text(temp, offset, 65, scale=1.5)
-                    spacing = self.lcd.measure_text(temp, 1.5) #, spacing, fixed_width)
-                    #print(spacing)
-                    msg="°"
-                    self.lcd.text(msg, offset+spacing, 43, scale=0.75)
-                    #lcd.character(176, spacing, 43, scale=1)
-                    spacing += self.lcd.measure_text(msg, 0.75)
-                    #print(spacing)
-                    msg = getattr(self.config, 'default_temp_unit')
-                    self.lcd.text(msg, offset+spacing, 65, scale=1.5)
-                    spacing += self.lcd.measure_text(msg, 1.5)
-                    #print(spacing)
-                    self.lcd.set_thickness(1)
-                    msg=f"RSSI:{tilt.rssi}"
-                    self.lcd.text(msg, 0, 100, scale=0.8)
-                    msg=f"{tilt.colour}"
-                    spacing = WIDTH - self.lcd.measure_text(msg, 0.8)
-                    msg=f"{tilt.colour}"
-                    self.lcd.text(msg, spacing, 100, scale=0.8)
-                    msg=f"{uncal_temp:.1f}  {uncal_sg:.{n}f}"
-                    self.lcd.text(msg, 0, 120, scale=0.8)
-                else:
-                    #we are missing data
-                    msg=f"Waiting for\nData"
-                    self.lcd.text(msg, 0, 65, scale=1)
-                gc.collect()
-                t1 = time.ticks_ms()
-                self.lcd.update()
-                t2 = time.ticks_ms()
-                # print(f"drawing took:{time.ticks_diff(t1, t_start)}, update took:{time.ticks_diff(t2, t1)}")
-                await asyncio.sleep(getattr(self.config, 'display_update_secs', 3))
-                og = self.config.get_original_gravity(tilt.colour)
-                #f tilt.extended:
-                #    # if og:
-                #    # draw a page of extended attributes, OG, ABV, attenuation
-                #    pass
-                index = (index + 1) % len(cards)
+                    await self.display_clock()
+            # show standard info for each configured tilt colour
+            extended_info = await self.display_sg_t(tilt, tilt_data_store)
+            # return a tilt_status object or None
+            if extended_info:
+                await self.display_extended(tilt, extended_info)
+            index = (index + 1) % len(cards)
 
     def read_latest_vals(self, tilt_data_store, tilt_colour):
         # read latest values & return a tuple
@@ -176,18 +101,15 @@ class LCD_Display:
             uncal_gravity = tilt_status.gravity
             tilt_status = TiltStatus(tilt_colour, uncal_tempF, uncal_SG, self.config, apply_calibration=True)
             # data offsets/calibration is applied here in TiltStatus
-            #tempC = tilt_status.temp_celsius
-            #SG = tilt_status.gravity
-            #uncal_SG *= 0.0001 if uncal_SG > 1200 else 0.001
         else:
             #logger.info(f"{colour} has no data")
             uncal_temp, uncal_gravity, tilt_status = None, None, None
         return (uncal_temp, uncal_gravity, tilt_status)
 
-    async def update_clock(self):
+    async def display_clock(self):
         # show a clock or a MOTD or something
-        #led.set_rgb(0, 0, 0)
-        WIDTH, HEIGHT = self.lcd.get_bounds()
+        width, height = self.lcd.get_bounds()
+        offset = None
         for _ in range(getattr(self.config, 'display_update_secs', 3)):
             self.lcd.set_pen(self.colour_to_palette["BG"])
             self.lcd.clear()
@@ -202,11 +124,115 @@ class LCD_Display:
             second = current_time[5]
             time_now = f"{hour:02d}:{minute:02d}:{second:02d}"
             #time_now = "16:27:00" # centred:
-            offset  = (WIDTH - self.lcd.measure_text(time_now, 1.5)) // 2
+            #offset  = (WIDTH - self.lcd.measure_text(time_now, 1.5)) // 2
+            offset = c_align(self.lcd, time_now, 1.5, width) if offset is None else offset
             self.lcd.text(time_now, offset, 65, scale=1.5)
             self.lcd.update()
             await asyncio.sleep(1)
-            # TODO subtract processing time from 1 second ticks_diff    
+            # TODO subtract processing time from 1 second ticks_diff
+
+    async def display_sg_t(self, tilt, tilt_data_store):
+        #
+        width, height = self.lcd.get_bounds()
+        #t_start = time.ticks_ms()
+        line1_v = 20
+        line2_v = 60
+        line3_v = 95 #100
+        line4_v = 125
+        uncal_temp, uncal_sg, tilt_values = self.read_latest_vals(tilt_data_store, tilt.colour)
+        n = 4 if tilt.hd else 3
+        self.lcd.set_pen(self.colour_to_palette["BG"])
+        self.lcd.clear()
+        self.lcd.set_pen(self.colour_to_palette[tilt.colour.upper()])
+        if all([uncal_temp, uncal_sg, tilt_values]):
+            self.lcd.set_thickness(3)
+            txt_scale = 1.5
+            # line1 gravity centred, SG:0.0000 or SG:0.000
+            msg=f"SG:{tilt_values.gravity:.{n}f}"
+            self.lcd.text(msg, c_align(self.lcd, msg, txt_scale, width), line1_v, scale=txt_scale)
+            # line2 temp centred, <value> <degree> <unit>, -10.3°C
+            msg = f"{tilt_values.temp_celsius:.1f}" if getattr(self.config, 'default_temp_unit') == "C" else f"{tilt_values.temp_fahrenheit:.1f}"
+            #msg = f"{msg}°{getattr(self.config, 'default_temp_unit')}"
+            h_offset = c_align(self.lcd, msg, txt_scale, width) - ((15+31)//2) # fudge for °C or °F
+            self.lcd.text(msg, h_offset, line2_v, scale=txt_scale)
+            spacing = self.lcd.measure_text(msg, scale=txt_scale)
+            msg="°"
+            self.lcd.text(msg, h_offset + spacing, line2_v - 15, scale=0.75)
+            msg = getattr(self.config, 'default_temp_unit')
+            self.lcd.text(msg, h_offset + spacing + 15, line2_v, scale=1.5)
+            self.lcd.set_thickness(1)
+            # line3 rssi left aligned, RSSI:-52
+            msg=f"RSSI:{tilt.rssi}"
+            self.lcd.text(msg, 0, line3_v, scale=1)#0.8)
+            # line3 tilt colour right aligned, blue
+            msg = tilt.colour
+            self.lcd.text(msg, r_align(self.lcd, msg, 1, width), line3_v, scale=1)#0.8)
+            # line4 uncal values & tx_power?
+            msg=f"{uncal_temp:.1f}"# {uncal_sg:.{n}f}"
+            msg2=f"{uncal_sg:.{n}f}"
+            msg3="52" # tilt.tx_power
+            self.lcd.text(msg, 0, line4_v, scale=1)#0.8)
+            # line4 tx_power field?
+            self.lcd.text(msg3, r_align(self.lcd, msg3, 1, width), line4_v, scale=1)#0.8)
+            # line 4 uncal sg centred beween temp & tx_power
+            spacing = (width - self.lcd.measure_text(f"{msg}{msg2}{msg3}", scale=1)) // 2
+            spacing += self.lcd.measure_text(f"{msg}", scale=1)
+            self.lcd.text(f"{uncal_sg:.{n}f}", spacing, line4_v, scale=1)
+        else:
+            # no data
+            self.lcd.set_thickness(2)
+            msg=f"waiting"
+            self.lcd.text(msg, c_align(self.lcd, msg, 1, width), 30, scale=1)
+            msg=f"for"
+            self.lcd.text(msg, c_align(self.lcd, msg, 1, width), 60, scale=1)
+            msg=f"data"
+            self.lcd.text(msg, c_align(self.lcd, msg, 1, width), 90, scale=1)
+            self.lcd.set_thickness(1)
+            msg=tilt.colour
+            self.lcd.text(msg, r_align(self.lcd, msg, 1, width), line4_v-5, scale=1)
+        gc.collect()
+        #t1 = time.ticks_ms()
+        self.lcd.update()
+        #t2 = time.ticks_ms()
+        #print(f"standard drawing took:{time.ticks_diff(t1, t_start)}, update took:{time.ticks_diff(t2, t1)}")
+        await asyncio.sleep(getattr(self.config, 'display_update_secs', 3))
+        return tilt_values if getattr(tilt_values, 'original_gravity', None) else None
+
+    async def display_extended(self, tilt, tilt_values):
+        #
+        self.lcd.set_pen(self.colour_to_palette["BG"])
+        self.lcd.clear()
+        self.lcd.set_pen(self.colour_to_palette[tilt.colour.upper()])
+        #t1 = time.ticks_ms()
+        width, height = self.lcd.get_bounds()
+        #t_start = time.ticks_ms()
+        line1_v = 20
+        line2_v = 60
+        line3_v = 95 #100
+        line4_v = 125
+        self.lcd.set_thickness(3)
+        # ABV
+        self.lcd.text(f"ABV:{tilt_values.alcohol_by_volume:.1f}%", 0, line1_v, scale=1.5)
+        # AA
+        msg="AA:100%"
+        spacing = 33 # display.lcd.measure_text("ABV", 1.5) - display.lcd.measure_text("AA", 1.5)
+        self.lcd.text(f"AA:{tilt_values.apparent_attenuation:.0f}%", spacing, line2_v, scale=1.5)
+        self.lcd.set_thickness(1)
+        # line3 rssi left aligned, RSSI:-52
+        msg=f"RSSI:{tilt.rssi}"
+        self.lcd.text(msg, 0, line3_v, scale=1)#0.8)
+        # tilt colour
+        msg=tilt.colour
+        self.lcd.text(msg, r_align(self.lcd, msg, 1, width), line3_v, scale=1)#0.8)
+        # OG
+        n = 4 if tilt.hd else 3
+        self.lcd.text(f"OG:{tilt_values.original_gravity:.{n}f}", 0, line4_v, scale=1)
+        gc.collect()
+        #t1 = time.ticks_ms()
+        self.lcd.update()
+        #t2 = time.ticks_ms()
+        #print(f"extended drawing took:{time.ticks_diff(t1, t_start)}, update took:{time.ticks_diff(t2, t1)}")
+        await asyncio.sleep(getattr(self.config, 'display_update_secs', 3))
 
 
 class RGB_Driver():
@@ -218,12 +244,6 @@ class RGB_Driver():
     def __init__(self, config: BridgeConfig):
         self.config = config
         self._led = None
-        #rgb_led = self._loadRGBLED(getattr(config, 'rgb_led_gpio', False))
-        #pins = getattr(config, 'rgb_led_gpio', False)
-        #if pins:
-        #    #self.rgb_led = self._loadRGBLED(6,7,8)
-        #    self._led=RGBLED(*pins)
-        #    #self._loadRGBLED(*pins)
         self._led = RGBLED(*pins) if (pins := getattr(config, 'rgb_led_gpio', None)) else None
         self._led and self._led.set_rgb(0, 0, 0)
     
@@ -231,13 +251,7 @@ class RGB_Driver():
         if pins:
             # print(f"***  {pins=}")
             self._led = RGBLED(*pins)
-            #self.set_rgb(100,0,0)
-            #asyncio.sleep_ms(500)
             self._led.set_rgb(0,0,0)
-            #self.rgb_task = asyncio.create_task(self._init_rgb_task())
-        #else:
-        #    rgb_led = None
-        #return rgb_led
 
     def off(self):
         # turn off
@@ -248,30 +262,25 @@ class RGB_Driver():
     #    self.rgb_led.set_rgb(0,0,0)
 
     async def _flash_rgb_task(self, rgb_colours):
-        #print("Hi we are here now")
+        #
         self._led.set_rgb(*rgb_colours)
         await asyncio.sleep_ms(500)
         self._led.set_rgb(0,0,0)
-    #while True:
-    #    asyncio sleep(8)
     
     def flash(self, colour):
         if self._led:
-            #if getattr(self, '_led_task', False):
-            #    self._led_task.cancel()
+            #
             if (_task := getattr(self, '_led_task', None)):
                 _task.cancel()
-            #self.rgb_task = asyncio.create_task(self._flash_rgb_task(colour))
-            #TODO load brightness here
-            #vals=get_color_values(colour)
-            #print(f"{vals=}")
-            #self._led.set_rgb(*vals)
-            #bns=getattr(self.config, 'rgb_brightness', 0.01)
-            #print(f"{bns=}")
-            #colval = get_color_values(colour, getattr(self.config, 'rgb_brightness', 0.5))
-            #print(f"{colval=}")
             self._led_task = asyncio.create_task(self._flash_rgb_task(get_color_values(colour, getattr(self.config, 'rgb_brightness', 0.1))))
-            #asyncio.sleep_ms(500)
-            #self._led.set_rgb(0,0,0)
 
-    
+
+def c_align(lcd_obj, txt, sz, width):
+    # return a offset from left 
+    return int((width - lcd_obj.measure_text(txt, sz)) // 2)
+
+def r_align(lcd_obj, txt, sz, width):
+    # return a offset from left 
+    return int(width - lcd_obj.measure_text(txt, sz))
+
+__version__ = '0.0.1'
